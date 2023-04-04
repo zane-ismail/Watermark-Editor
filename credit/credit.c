f7df86554f1d8256b7b497652bb3f7b72a24979a
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-04 16:42:13 +0000

    Tue, Apr 4, 2023, 5:42 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -13,8 +13,8 @@ int main(void)
     long card_length = get_card_length(card);
     if (card_length > 12 && card_length < 17)
     {
-        long first_digits = get_card_type(card, card_length);
         long card_digits = validate_card(card, card_length);
+        long first_digits = get_card_type(card, card_length);
         if (card_digits == 1)
         {
             printf("INVALID\n");
@@ -46,6 +46,60 @@ long get_card_length(long card)
         printf("INVALID\n");
     }
     return count;
+}
+
+// Luhn's algorithm
+long validate_card(long card, long count)
+{
+    int n = 0;
+    long modular_a = 100;
+    long modular_b = 10;
+    long divide1 = 10;
+    long divide2 = 1;
+    int cycle = count / 2;
+    int sum = 0;
+    int i;
+    int j;
+
+// Iterate to find the second to last digit and every other digit after that
+    do
+    {
+        long cc = card;
+        cc = cc % modular_a;
+        i = cc / divide1;
+        modular_a = modular_a * 100;
+        divide1 = divide1 * 100;
+// Multiply digit by 2
+        i = (i * 2);
+// Split 2 digit numbers into single digits
+        if (i > 9)
+        {
+            i = (i / 10) + (i % 10);
+        }
+// Add all single digits together
+        sum = sum + i;
+//Rounds up for cards with an odd number of digits
+        if (cycle % 10 > 0)
+        {
+            cycle ++;
+        }
+// Find the last digit and every other digit after that
+        cc = cc % modular_b;
+        j = cc / divide2;
+        modular_b = modular_b * 100;
+        divide2 = divide2 * 100;
+// Add all digits together
+        sum = sum + j;
+        n++;
+    }
+// Iterates through loop until count is complete
+    while (n != cycle);
+// Validate modulo 10 is congruent to 0
+    if (sum % 10 == 0)
+    {
+        return 0;
+    }
+    return 1;
 }
 
 // Identify card type
@@ -108,58 +162,3 @@ long get_card_type(long card, long count)
     }
     return 0;
 }
-
-// Luhn's algorithm
-long validate_card(long card, long count)
-{
-    int n = 0;
-    long modular_a = 100;
-    long modular_b = 10;
-    long divide1 = 10;
-    long divide2 = 1;
-    int cycle = count / 2;
-    int sum = 0;
-    int i;
-    int j;
-
-// Iterate to find the second to last digit and every other digit after that
-    do
-    {
-        long cc = card;
-        cc = cc % modular_a;
-        i = cc / divide1;
-        modular_a = modular_a * 100;
-        divide1 = divide1 * 100;
-// Multiply digit by 2
-        i = (i * 2);
-// Split 2 digit numbers into single digits
-        if (i > 9)
-        {
-            i = (i / 10) + (i % 10);
-        }
-// Add all single digits together
-        sum = sum + i;
-//Rounds up for cards with an odd number of digits
-        if (cycle % 10 > 0)
-        {
-            cycle ++;
-        }
-// Find the last digit and every other digit after that
-        cc = cc % modular_b;
-        j = cc / divide2;
-        modular_b = modular_b * 100;
-        divide2 = divide2 * 100;
-// Add all digits together
-        sum = sum + j;
-        n++;
-    }
-// Iterates through loop until count is complete
-    while (n != cycle);
-// Validate modulo 10 is congruent to 0
-    if (sum % 10 == 0)
-    {
-        return 0;
-    }
-    return 1;
-}
-
