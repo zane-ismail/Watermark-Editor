015eee22305c22c45469a067ba7d1a913d4cce9b
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-04 12:16:30 +0000

    Tue, Apr 4, 2023, 1:16 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,11 +11,7 @@ int main(void)
 {
     long card = get_card_number();
     long card_length = get_card_length(card);
-    if (card_length == 1)
-    {
-        printf("INVALID\n");
-    }
-    else
+    if (card_length > 12 && card_length < 17)
     {
         long first_digits = get_card_type(card, card_length);
         long card_digits = validate_card(card, card_length);
@@ -48,7 +44,7 @@ long get_card_length(long card)
     while (cc > 0);
     if (count < 13 || count > 16)
     {
-        return 1;
+        printf("INVALID\n");
 
     }
     return count;
@@ -99,7 +95,7 @@ long get_card_type(long card, long count)
         }
         else
         {
-            return 1;
+            printf("INVALID\n");
         }
     }
     else if (count == 16)
@@ -116,7 +112,7 @@ long get_card_type(long card, long count)
         }
         else
         {
-            return 1;
+            printf("INVALID\n");
         }
     }
     else if (count == 13)
@@ -128,7 +124,7 @@ long get_card_type(long card, long count)
         }
         else
         {
-            return 1;
+            printf("INVALID\n");
         }
     }
     return 0;
@@ -139,60 +135,56 @@ long validate_card(long card, long count)
 {
     int m = 0;
     int n = 0;
-    long modular_a = 100;
-    long modular_b = 10;
-    long divide_a = 10;
-    long divide_b = 1;
+    long modular1 = 100;
+    long modular2 = 10;
+    long divide1 = 10;
+    long divide2 = 1;
     long digit1 = 0;
     long digit2 = 0;
     long digit3 = 0;
-    long int_b1 = 0;
-    long int_b2 = 0;
+    long d21 = 0;
+    long d22 = 0;
     long luhns_a;
     long digit4;
     long cycle = count / 2;
-    int int_a;
-    int int_b;
-    int int_c;
-    int int_d;
+    long d1;
+    long d2;
+    long d3;
+    long d4;
     do
     {
         long a = card;
-// Find the second to last digit and every other digit after that
-        a = a % modular_a;
-        int_a = a / divide_a;
-        modular_a = modular_b * 100;
-        divide_a = divide_a * 100;
-// Multiply digit by 2 and add together
-        int_b = (int_a * 2);
-// Split 2 digit numbers into single digits
-        if (int_b > 9)
+        // Find the second to last digit and every other digit after that
+        a = a % modular1;
+        d1 = a / divide1;
+        modular1 = modular1 * 100;
+        divide1 = divide1 * 100;
+        // Multiply digit by 2 and add together
+        d2 = (d1 * 2);
+        if (d2 > 9)
         {
-            int_b = (int_b / 10) + (int_b % 10);
+            d2 = (d2 / 10) + (d2 % 10);
         }
-        digit1 = digit1 + int_b;
+        digit1 = digit1 + d2;
         digit4 = digit1 + digit3;
-//Rounds up for cards with an odd number of digits
         if (cycle % 10 > 0)
         {
             cycle ++;
         }
-// Find the last digit and every other digit after that
+        // Find the last digit and every other digit after that
         {
-        a = a % modular_b;
-        int_c = a / divide_b;
-        modular_b = modular_b * 100;
-        divide_b = divide_b * 100;
+        a = a % modular2;
+        d3 = a / divide2;
+        modular2 = modular2 * 100;
+        divide2 = divide2 * 100;
         // Multiply digit by 2 and add together
-        digit2 = (digit2 + int_c);
+        digit2 = (digit2 + d3);
         m++;
         }
-
     }
     while (m != cycle);
-    digit3 = digit3 + int_b1 + int_b2;
+    digit3 = digit3 + d21 + d22;
     luhns_a = (digit1 + digit2);
-    printf("%ld\n", luhns_a);
     if (luhns_a % 10 == 0)
     {
         return 0;
