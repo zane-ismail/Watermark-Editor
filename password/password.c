c4cc99f2205a6f0b1b3548ce8b374ef844ff27aa
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-12 15:52:56 +0000

    Wed, Apr 12, 2023, 4:52 PM +01:00

--- password/password.c
+++ password/password.c
@@ -27,8 +27,24 @@ bool valid(string password)
     for (i = 0; password[i] != '\0'; i++)
     printf("%i\n", password[i]);
 // Validate upper case
-    if (password[i] => 97 && password[i] =< 122)
+    if (password[i] >= 65 && password[i] <= 90)
+    {
+        return true;
+    }
 // Validate lower case
-    else if (password[i] => 97 && password[i] =< 122)
+    else if (password[i] >= 97 && password[i] <= 122)
+    {
+        return true;
+    }
+// Validate number
+    else if (password[i] >= 48 && password[i] <= 57)
+    {
+        return true;
+    }
+// Validate symbol
+    else if (password[i] >= 32 && password[i] <= 47) || (password[i] >= 58 && password[i] <= 126)
+    {
+        return true;
+    }
     return false;
 }
