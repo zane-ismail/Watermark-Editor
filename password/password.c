756f225b63c96fdac11b0c60ed8ebf0563f2662b
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-12 16:07:37 +0000

    Wed, Apr 12, 2023, 5:07 PM +01:00

--- password/password.c
+++ password/password.c
@@ -24,31 +24,44 @@ int main(void)
 bool valid(string password)
 {
     int i;
+    bool upper = 0;
+    bool lower = 0;
+    bool number = 0;
+    bool symbol = 0;
     for (i = 0; password[i] != '\0'; i++)
-    printf("%i\n", password[i]);
-// Validate upper case
-    if (password[i] >= 65 && password[i] <= 90)
     {
-        printf("Upper Case");
+    // Validate upper case
+        if (password[i] >= 65 && password[i] <= 90)
+        {
+            upper = 1;
+        }
+    // Validate lower case
+        else if (password[i] >= 97 && password[i] <= 122)
+        {
+            lower = 1;
+        }
+    // Validate number
+        else if (password[i] >= 48 && password[i] <= 57)
+        {
+            number = 1;
+        }
+    // Validate symbol
+        else if (password[i] >= 32 && password[i] <= 47)
+        {
+            symbol = 1;
+        }
+        else if (password[i] >= 58 && password[i] <= 126)
+        {
+            symbol = 1;
+        }
+    printf("\n");
     }
-// Validate lower case
-    else if (password[i] >= 97 && password[i] <= 122)
+    if (upper == 1 && lower == 1 && number == 1 && symbol == 1)
     {
-        printf("Lower Case");
+        return true;
     }
-// Validate number
-    else if (password[i] >= 48 && password[i] <= 57)
+    else
     {
-        printf("Number");
+        return false;
     }
-// Validate symbol
-    else if (password[i] >= 32 && password[i] <= 47)
-    {
-        printf("Symbol1");
-    }
-    else if (password[i] >= 58 && password[i] <= 126)
-    {
-        printf("Symbol2");
-    }
-    return false;
 }
