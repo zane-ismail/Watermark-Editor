01a21019c0d40c19973bfe1ca360faa206775383
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-15 20:07:05 +0000

    Sat, Apr 15, 2023, 9:07 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -47,29 +47,29 @@ int main(int argc, string argv[])
             {
                 character = (plaintext[i] - ASCII_UPP_START);
                 character = key[character];
-                if (key[i] >= ASCII_LOW_START)
+                if (character >= ASCII_LOW_START)
                 {
                     character = (character - 32);
-                    printf("!");
+                    // printf("!");
                 }
                 else
                 {
-                    printf(",");
+                    // printf(",");
                 }
             }
             else if (character >= ASCII_LOW_START && character <= ASCII_LOW_END)
             {
                 character = (plaintext[i] - ASCII_LOW_START);
                 character = key[character];
-                if (key[character] <= ASCII_UPP_END)
+                if (character <= ASCII_UPP_END)
                 {
-                    printf(".%i.", key[character]);
+                    // printf(".%i.", key[character]);
                     character = (character + 32);
                     // printf(".%i.", key[i]);
                 }
                 else
                 {
-                    printf(".");
+                    // printf(".");
                 }
             }
             else
