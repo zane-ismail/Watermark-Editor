aa7534fc1c7792bbae2e0aa3ebc4b849217ddb57
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-15 18:19:35 +0000

    Sat, Apr 15, 2023, 7:19 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -47,12 +47,12 @@ int main(int argc, string argv[])
             // Validation for input characters
             if (character >= ASCII_UPP_START && character <= ASCII_UPP_END)
             {
-                // if (key[i] >= ASCII_LOW_START && key[i] <= ASCII_LOW_END)
-                // {
-                //     character = (plaintext[i] - ASCII_UPP_START);
-                //     character = key[character];
-                //     character = (character - 32);
-                // }
+                if (key[i] >= ASCII_LOW_START && key[i] <= ASCII_LOW_END)
+                {
+                    character = (plaintext[i] - ASCII_UPP_START);
+                    character = key[character];
+                    character = (character - 32);
+                }
                 if (key[i] >= ASCII_LOW_START)
                 {
                     character = (plaintext[i] - ASCII_UPP_START);
