9d91b906b4268654d771d04ec1d6c4a1cf04ba12
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-16 21:24:32 +0000

    Sun, Apr 16, 2023, 10:24 PM +01:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -85,7 +85,7 @@ int main(int argc, string argv[])
         string guess = get_guess(wordsize);
 
         // array to hold guess status, initially set to zero
-        int status[8];
+        int status[7];
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
         for (int j = 0; j < wordsize; j++)
