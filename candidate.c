54b31ab8f094a4a6c96bc63e668ab27be5c74f05
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-18 12:19:34 +0000

    Tue, Apr 18, 2023, 1:19 PM +01:00

--- candidate.c
+++ candidate.c
@@ -11,9 +11,16 @@ candidate;
 int main(void)
 {
     candidate president = get_candidate("Enter a candidate: ")
+    printf("%s\n", president.name);
+    printf("%i\n", president.votes);
 }
 
 candidate get candidate(string prompt)
 {
+    print("%s\n", prompt);
 
+    candidate c;
+    c.name = get_string("Enter a name: ");
+    c.votes = get_int("Enter a number of votes:);
+    return c;
 }
