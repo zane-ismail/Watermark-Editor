8a07e25eef94bc7cf391413a75b124e425d76822
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-18 18:47:07 +0000

    Tue, Apr 18, 2023, 7:47 PM +01:00

---
+++ atoi/atoi.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+int number = 0;
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    int count = strlen(input);
+    int tempint = 0;
+
+    if (count == 0)
+    {
+        return number;
+    }
+    tempint = input[count-1] - '0';
+    input[count-1] = '\0';
+    convert(input);
+    number = number * 10 + tempint;
+    return number;
+}
