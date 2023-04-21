1c8c849a443067992953aa3a0f4cb32060267c88
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-21 17:50:51 +0000

    Fri, Apr 21, 2023, 6:50 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,9 +19,21 @@ int main(void)
     float S = count_sentences(text) / count_words(text) * 100;
     int index = round(0.0588 * L - 0.296 * S - 15.8);
 
-    printf("INDEX: %i\n", index);
-
-    if (index ==)
+    // if index number is less than 1, output "Before Grade 1"
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+    // if index number is 16 or higher, output
+    else if (index >= 16)
+    {
+        printf("Grade 16+");
+    }
+    // print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
 int count_letters(string text)
@@ -35,7 +47,7 @@ int count_letters(string text)
                 l_count++;
             }
         }
-    printf("%i letters\n", l_count);
+    // printf("%i letters\n", l_count);
     return l_count;
 }
 
@@ -65,11 +77,9 @@ int count_sentences(string text)
             s_count++;
         }
     }
-    printf("%i sentences\n", s_count);
+    // printf("%i sentences\n", s_count);
     return s_count;
 }
-    // print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer
 
-    // if index number is 16 or higher, output "Grade 16+"
 
-    // if index number is less than 1, output "Before Grade 1"
+
