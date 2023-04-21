f096ca8f73671f80e633969c09e16eb9e7202afd
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-21 16:31:46 +0000

    Fri, Apr 21, 2023, 5:31 PM +01:00

---
+++ readability/readability.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+    // prompt the user for a string of text using get_string.
+    string text = get_string("Text: ");
+}
+
+int count_letters(string text)
+{
+    // count letters
+    int txt_len = strlen(text);
+    printf("LENGTH: %i\n", txt_len);
+
+    // count words
+
+
+}
+
+
+
+
+    // count sentences
+
+    // a letter is any lowercase character from a to z or any uppercase character from A to Z
+
+    // a word is any sequence of characters separated by spaces
+
+    // any occurrence of a period, exclamation point, or question mark indicates the end of a sentence
+
+
+    // print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer
+
+    // if index number is 16 or higher, output "Grade 16+"
+
+    // if index number is less than 1, output "Before Grade 1"
