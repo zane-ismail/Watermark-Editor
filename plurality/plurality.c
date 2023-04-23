d0e13354da3757fcb2f6bddfe6bb185b2e128a46
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-23 10:24:06 +0000

    Sun, Apr 23, 2023, 11:24 AM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -102,14 +102,14 @@ void print_winner(void)
                 winner_a = candidates[j].name;
                 max = candidates[j].votes;
                 printf("%s\n", winner_a);
-                if (candidates[j].votes == max)
-                    {
-                        winner_b = winner_a;
-                        winner_a = candidates[j].name;
-                        max = candidates[j].votes;
-                        printf("%s ", winner_a);
-                        printf("%s\n", winner_b);
-                    }
+            }
+            else if (candidates[j].votes == max)
+            {
+                winner_b = winner_a;
+                winner_a = candidates[j].name;
+                max = candidates[j].votes;
+                printf("%s ", winner_a);
+                printf("%s\n", winner_b);
             }
         }
     }
