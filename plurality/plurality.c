808e2cc8a6344bc8dd0f89a87fdef691191fea76
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-23 12:45:33 +0000

    Sun, Apr 23, 2023, 1:45 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -74,15 +74,16 @@ bool vote(string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
+            return false;
+        }
+        else if (strcmp(name, candidates[i].name) == 0)
+        {
             candidates[i].votes++;
             is_match = true;
             // The vote function in this case should return true to indicate a successful ballot
             return true;
         }
-        else if (i == MAX && is_match == false)
-        {
-            return false;
-        }
+
         // If name does not match the name of any of the candidates in the election, no vote totals should change, and the vote function should return false to indicate an invalid ballot
     }
     // You may assume that no two candidates will have the same name
