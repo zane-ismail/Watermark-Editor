d4b22319ebcf5de93838b512aa4b722f2001facd
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-26 00:25:49 +0000

    Wed, Apr 26, 2023, 1:25 AM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -162,7 +162,7 @@ void tabulate(void)
                 }
                 if (preferences[j][0] == i && candidates[i].eliminated)
                 {
-                    for (int k = ; k < candidate_count; k++)
+                    for (int k = 1; k < candidate_count; k++)
                     {
                         if (preferences[j][k] == i)
                         {
