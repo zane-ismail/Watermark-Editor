2375e04e3a234c79084c2196774a0c0b73effaa8
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-30 14:19:09 +0000

    Sun, Apr 30, 2023, 3:19 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -160,19 +160,19 @@ void add_pairs(void)
             if (preferences[i][j] > preferences[j][i])
             {
                 // The pairs should thus all be stored between pairs[0] and pairs[pair_count - 1], inclusive)
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
                 printf("1Winner: %d\n", pairs[pair_count].loser);
                 printf("1Loser: %d\n", pairs[pair_count].winner);
-                pairs[pair_count].winner = i;
-                pairs[pair_count].loser = j;
-                pair_count++;
                 // update the global variable pair_count to be the number of pairs of candidates
+                pair_count++;
             }
             else if (preferences[i][j] < preferences[j][i])
             {
-                printf("2Winner: %d\n", pairs[pair_count].loser);
-                printf("2Loser: %d\n", pairs[pair_count].winner);
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
+                printf("1Winner: %d\n", pairs[pair_count].loser);
+                printf("1Loser: %d\n", pairs[pair_count].winner);
                 pair_count++;
             }
         }
