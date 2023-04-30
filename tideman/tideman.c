17589b8dc54ceae49b902cd8333dbd5ad43ab16c
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-30 16:59:51 +0000

    Sun, Apr 30, 2023, 5:59 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -192,8 +192,8 @@ void sort_pairs(void)
                 {
                     if (temp_max_count > max_count)
                     {
-                        max_count = i;
-                        pairs[i].winner = i;
+                        max_count = j;
+                        pairs[i].winner = j;
                     }
                 }
             max_count = 0;
