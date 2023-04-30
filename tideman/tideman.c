dd9588cdb93a3a68a2126a5817aee929e03fe33e
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-30 12:57:17 +0000

    Sun, Apr 30, 2023, 1:57 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -130,9 +130,9 @@ void record_preferences(int ranks[])
                 {
                     preferences[ranks[j]][i]++;
                 }
-                printf("%i: ", i);
-                printf("%i:  ", j);
-                printf("%i\n", preferences[i][ranks[i]]);
+                // printf("%i: ", i);
+                // printf("%i:  ", j);
+                // printf("%i\n", preferences[i][ranks[i]]);
             }
     }
     return;
@@ -143,15 +143,15 @@ void add_pairs(void)
 {
     // TODO
     // add all pairs of candidates where one candidate is preferred to the pairs array
-    printf("A/A: %i\n", preferences[0][0]);
-    printf("A/B: %i\n", preferences[0][1]);
-    printf("A/C: %i\n", preferences[0][2]);
-    printf("B/A: %i\n", preferences[1][0]);
-    printf("B/B: %i\n", preferences[1][1]);
-    printf("B/C: %i\n", preferences[1][2]);
-    printf("C/A: %i\n", preferences[2][0]);
-    printf("C/B: %i\n", preferences[2][1]);
-    printf("C/C: %i\n", preferences[2][2]);
+    // printf("A/A: %i\n", preferences[0][0]);
+    // printf("A/B: %i\n", preferences[0][1]);
+    // printf("A/C: %i\n", preferences[0][2]);
+    // printf("B/A: %i\n", preferences[1][0]);
+    // printf("B/B: %i\n", preferences[1][1]);
+    // printf("B/C: %i\n", preferences[1][2]);
+    // printf("C/A: %i\n", preferences[2][0]);
+    // printf("C/B: %i\n", preferences[2][1]);
+    // printf("C/C: %i\n", preferences[2][2]);
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
