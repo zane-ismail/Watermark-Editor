02c70270243114eb7ff07cc8aeae86b6b11d459c
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-04-30 17:06:52 +0000

    Sun, Apr 30, 2023, 6:06 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -188,7 +188,7 @@ void sort_pairs(void)
             for (int j = 0; j < candidate_count; j++)
             {
                 // a pair of candidates who are tied (one is not preferred over the other) should not be added to the array
-                temp_max_count = preferences[i][j] - preferences[j][i];
+                temp_max_count = preferences[pairs[i].winner][j] - preferences[pairs[i].winner][i];
                 {
                     if (temp_max_count > max_count)
                     {
