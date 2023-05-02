f5b6db4fd1b7f9b791908bf13c5fd111f74a14bf
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-02 11:49:25 +0000

    Tue, May 2, 2023, 12:49 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -231,7 +231,7 @@ bool recursion(int w, int l)
     for (int i = 0; i < locked_count; i++)
     {
         // check 1) the winner leads back to the loser
-        if (locked[w][l] == true)
+        if (locked[pairs[i][i]] == true)
         {
             return true;
         }
