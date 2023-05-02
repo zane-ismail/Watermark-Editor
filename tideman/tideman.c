df647b8b486c5797e449989543d9a11db385974b
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-02 13:49:07 +0000

    Tue, May 2, 2023, 2:49 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -238,7 +238,7 @@ void lock_pairs(void)
 bool recursion(int w, int l)
 {
     // check if original new pair winner is the same as a recurive pair loser
-    if (l == ORIGINAL)
+    if (w == ORIGINAL)
     {
         return true;
     }
