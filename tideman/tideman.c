638bb13ce43b214f6d953de026c52aada60578c1
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-02 14:27:37 +0000

    Tue, May 2, 2023, 3:27 PM +01:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -249,12 +249,16 @@ bool recursion(int w, int l)
     // check the previous is locked
         if (locked[pairs[i].winner][pairs[i].loser] == true)
         {
-            // checks if new pair loser is the same as previous pair winner
-            if (l == pairs[i].winner)
+            do
             {
-                // checks if new pair winner is the same as previous pair loser
-                recursion(pairs[i].loser, pairs[i].loser);
+                // checks if new pair loser is the same as previous pair winner
+                if (l == pairs[i].winner)
+                {
+                    // checks if new pair winner is the same as previous pair loser
+                    recursion(pairs[i].loser, pairs[i].loser);
+                }
             }
+            while (l == pairs[i].winner);
         }
     }
     return false;
