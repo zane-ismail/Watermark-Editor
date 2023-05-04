559f358d42ed5b409874b7ccbb3587767d72ab4c
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-04 18:37:50 +0000

    Thu, May 4, 2023, 7:37 PM +01:00

--- license/license.c
+++ license/license.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     }
 
     // Create buffer to read into
-    char buffer[28];
+    char buffer[7];
 
     // Create array to store plate numbers
     char *plates[8];
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
     int idx = 0;
 
-    while (fread(buffer, 6, 7, infile) == 7)
+    while (fread(buffer, 1, 7, infile) == 7)
     {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
