186de57feb739891d5bd3d684c8624b21ea05ced
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-09 16:55:51 +0000

    Tue, May 9, 2023, 5:55 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -17,7 +17,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
                 double average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
                 int avg = round(average);
 
-                image[i][j] = avg;
+                image[i][j].rgbtBlue = avg;
+                image[i][j].rgbtGreen = avg;
+                image[i][j].rgbtRed = avg;
             }
         }
     }
