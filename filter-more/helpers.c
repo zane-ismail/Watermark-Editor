a1a851d244394915ad9c8fc6a8a0e505f34a2dd2
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-09 14:19:32 +0000

    Tue, May 9, 2023, 3:19 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -29,7 +29,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // Store values in temporary variable
-    RGBTRIPLE temp[height][width];
+    temp[height][width];
     int x = 1;
 
     // begin with right most pixel and work left
@@ -37,7 +37,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE temp[i][j] = image[i][width - x].rgbtBlue;
+            temp[i][j] = image[i][width - x];
             x++;
             // reset x value at the end (leftside) of the row
             if (x == width + 1)
@@ -52,9 +52,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = tempblue[i][j];
-            image[i][j].rgbtGreen = tempgreen[i][j];
-            image[i][j].rgbtRed = tempred[i][j];
+            image[i][j] = temp;
         }
     }
 }
@@ -63,7 +61,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     // Store values in temporary variable
-    double RGBTRIPLE temp[height][width];
+    RGBTRIPLE temp[height][width];
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -71,7 +69,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // top left corner
             if (i == 0 && j == 0)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i][j + 1] + image[i + 1][j] +
+                temp[i][j] = (image[i][j] + image[i][j + 1] + image[i + 1][j] +
                                       image[i + 1][j + 1] / 4.0;
             }
             // top right corner
@@ -84,43 +82,43 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // bottom left corner
             else if (i == height - 1 && j == 0)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j + 1] +
+                temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j + 1] +
                                       image[i][j + 1]) / 4.0;
             }
             // bottom right corner
             else if (i == height - 1 && j == width - 1)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
+                temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
                                       image[i][j - 1]) / 4.0;
             }
             // top row
             else if (i == 0)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i][j - 1] + image[i][j + 1] +
+                temp[i][j] = (image[i][j] + image[i][j - 1] + image[i][j + 1] +
                                       image[i + 1][j - 1] + image[i + 1][j] + image[i + 1][j + 1]) / 6.0;
             }
             // left column
             else if (j == 0)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i][j + 1] + image[i + 1][j] +
+                temp[i][j] = (image[i][j] + image[i][j + 1] + image[i + 1][j] +
                                       image[i + 1][j + 1] + image[i - 1][j] + image[i - 1][j + 1]) / 6.0;
             }
             // bottom row
             else if (i == height - 1)
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i][j - 1] + image[i][j + 1] +
+                temp[i][j] = (image[i][j] + image[i][j - 1] + image[i][j + 1] +
                                       image[i - 1][j - 1] + image[i - 1][j] + image[i - 1][j + 1]) / 6.0;
             }
             // right column
             else if (j == width - 1)
 
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
+                temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
                                       image[i][j - 1] + image[i + 1][j] + image[i + 1][j - 1]) / 6.0;
             }
             // middle
             else
             {
-                RGBTRIPLE temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
+                temp[i][j] = (image[i][j] + image[i - 1][j] + image[i - 1][j - 1] +
                                       image[i][j - 1] + image[i + 1][j] + image[i + 1][j - 1] +
                                       image[i + 1][j + 1] + image[i][j + 1] + image[i - 1][j + 1]) / 9.0;
             }
@@ -132,7 +130,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = round(RGBTRIPLE temp[i][j]);
+            image[i][j] = round(temp[i][j]);
         }
     }
 }
