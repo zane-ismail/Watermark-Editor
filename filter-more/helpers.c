016c506f8bab490546d023b22e3eb16237e5a3c6
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-13 11:22:05 +0000

    Sat, May 13, 2023, 12:22 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -82,11 +82,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int y = -1; y < 2; y++)
                 {
-                    if (i + x < 0 || height + y >= height)
+                    if (i + x < 0 || i + y >= height)
                     {
                         continue;
                     }
-                    if (j + y < 0 || width + x >= width)
+                    if (j + y < 0 || j + x >= width)
                     {
                         continue;
                     }
