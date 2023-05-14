affc129ec07464dc05edea9343ad9e3fc4084940
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-14 14:57:51 +0000

    Sun, May 14, 2023, 3:57 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,12 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-
-    Implement your program in a file called recover.c in a directory called recover.
-    Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
-    If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
-    If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
-    The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
-    Your program, if it uses malloc, must not leak any memory.
+    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
+    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
+    // The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
+    // Your program, if it uses malloc, must not leak any memory.
 
 }
