9b88916eb730307bfba777810fce264605fab63a
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-14 15:32:43 +0000

    Sun, May 14, 2023, 4:32 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
 
-    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
     // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
     // The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
     // Your program, if it uses malloc, must not leak any memory.
 
-    while (argc != 2)
+    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
+    if (argc != 2)
     {
         printf("Usage: ./recover [input file]");
         return 1;
     }
-    char *input = argc[1];
+    char *input = argv[1];
     // open memory card
     // fread(data, size, number, input)
     // repeat until end  of card:
