6450b50aa93b68e88b982fd48d790234c530aee7
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-05-14 15:02:10 +0000

    Sun, May 14, 2023, 4:02 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    fread(data, size, number, input)
     // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
     // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
     // The files you generate should each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
