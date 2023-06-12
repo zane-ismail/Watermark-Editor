be1373d3bb231b5a79ed7668138b7cbb6c068ceb
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 19:08:41 +0000

    Mon, Jun 12, 2023, 8:08 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,12 +35,13 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
         {
+            b_count++;
+        }
             FILE *output;
             sprintf(filename, "%03i.jpg", count);
             FILE *img = fopen(filename, "w");
             fwrite(filename, sizeof(char), 1, img);
             count++;
-            b_count++;
         }
         else if (buffer[b_count] == 0xff && buffer[b_count + 1] == 0xd8 && buffer[b_count + 2] == 0xff && (buffer[b_count + 3] & 0xf0) == 0xe0)
 
