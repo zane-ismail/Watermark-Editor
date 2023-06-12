49fe861fed075a5d3da637a4dd40f3200db78b27
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 21:11:54 +0000

    Mon, Jun 12, 2023, 10:11 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,11 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    int count = -1;
+    int count = - 1;
     int b_count = 0;
     char *input = argv[1];
     char *filename = malloc(8 * sizeof(char));
     int BLOCK_SIZE = 512;
+    unsigned char buffer[BLOCK_SIZE];
     FILE *img;
 
 
@@ -26,13 +27,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    unsigned char buffer[512];
     // read 512 bytes into a buffer & repeat until end of card
     while (fread(buffer, sizeof(char), BLOCK_SIZE, f) == BLOCK_SIZE)
         // if start of new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             count++;
+            printf("%i\n", count);
             // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
             sprintf(filename, "%03i.jpg", count);
             img = fopen(filename, "w");
@@ -54,6 +55,5 @@ int main(int argc, char *argv[])
 
     // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
     // Your program, if it uses malloc, must not leak any memory.
-    free();
 
 }
