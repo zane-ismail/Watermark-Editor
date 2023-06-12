97d2b9dc8c9fb529c5e6b7c3d0c0400c072633fa
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 20:11:26 +0000

    Mon, Jun 12, 2023, 9:11 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     char *input = argv[1];
     char *filename = malloc(8 * sizeof(char));
     int BLOCK_SIZE = 512;
-    unsigned char buffer[512];
 
     // Accept exactly one command-line argument. If not, remind user of correct usage, and return 1.
     if (argc != 2)
@@ -27,9 +26,12 @@ int main(int argc, char *argv[])
     // repeat until end of card
     while ()
     {
+        // read 512 bytes into a buffer
+        unsigned char buffer[512];
         // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
         sprintf(filename, "%03i.jpg", count);
-        FILE *img = fopen(filename, "w");// read 512 bytes into a buffer
+        FILE *img = fopen(filename, "w");
+        
         while (fread(buffer, sizeof(char), BLOCK_SIZE, f) == BLOCK_SIZE)
         // if start of new JPEG
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
