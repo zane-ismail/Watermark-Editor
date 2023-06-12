70bb54633ea536018c375158e0bea65e803df2b5
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 21:01:03 +0000

    Mon, Jun 12, 2023, 10:01 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,9 +26,8 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // read 512 bytes into a buffer
     unsigned char buffer[512];
-    // repeat until end of card
+    // read 512 bytes into a buffer & repeat until end of card
     while (fread(buffer, sizeof(char), BLOCK_SIZE, f) == BLOCK_SIZE)
         // if start of new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
