a995e7d0a9ecbd121d024df6e771ebe58fee3572
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 19:16:02 +0000

    Mon, Jun 12, 2023, 8:16 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     int b_count = 0;
     char *input = argv[1];
     char *filename = malloc(8 * sizeof(char));
+    int block = 512;
     // read 512 bytes into a buffer
     unsigned char buffer[512];
 
@@ -27,7 +28,7 @@ int main(int argc, char *argv[])
 
 
     // repeat until end of card:
-    while (fread(buffer, sizeof(char), 512, f) != 0)
+    while (fread(buffer, sizeof(char), block, f) != 0)
 
         // if start of new JPEG
 
@@ -35,15 +36,14 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
         {
-            b_count++;
-        }
             FILE *output;
             sprintf(filename, "%03i.jpg", count);
             FILE *img = fopen(filename, "w");
-            fwrite(filename, sizeof(char), 1, img);
+            fwrite(filename, sizeof(char), block, img);
             count++;
         }
-        else if (buffer[b_count] == 0xff && buffer[b_count + 1] == 0xd8 && buffer[b_count + 2] == 0xff && (buffer[b_count + 3] & 0xf0) == 0xe0)
+
+        // else if (buffer[b_count] == 0xff && buffer[b_count + 1] == 0xd8 && buffer[b_count + 2] == 0xff && (buffer[b_count + 3] & 0xf0) == 0xe0)
 
 
             // else
