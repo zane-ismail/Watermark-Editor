fc048245da9512bd51efc092ef545a5ccb751383
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-12 20:22:51 +0000

    Mon, Jun 12, 2023, 9:22 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -42,16 +42,16 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
+                    fclose(img);
+                    // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
+                    sprintf(filename, "%03i.jpg", count);
+                    FILE *img = fopen(filename, "w");
                 }
             else
             {
-                
+
             }
 
-            fclose(img);
-            // Files are each be named ###.jpg, where ### is a three-digit decimal number, starting with 000 for the first image and counting up.
-            sprintf(filename, "%03i.jpg", count);
-            FILE *img = fopen(filename, "w");
             }
 
         count++;
