97c1dfa484d88deb99abfe89cae80f7b93bc9493
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-13 15:39:59 +0000

    Tue, Jun 13, 2023, 4:39 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -11,7 +11,10 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-    
+    if (argc != 3)
+    {
+        fprint("")
+    }
 
     // Open input file for reading
     // TODO #2
