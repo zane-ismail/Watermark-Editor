cb7178e7be52e2c5071dfab2853ad51169579b80
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-13 12:25:04 +0000

    Tue, Jun 13, 2023, 1:25 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     sprintf(filename, "%03i.jpg", count);
     img = fopen(filename, "w");
     // read 512 bytes into a buffer & repeat until end of card
-    while (fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE)
+    while (fread(buffer, sizeof(char), BLOCK_SIZE, card) == BLOCK_SIZE)
     {
         // if start of new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
