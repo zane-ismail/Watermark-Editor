10249b9f4590cfc00204d7f56ee9d581a6abbf54
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-13 20:03:03 +0000

    Tue, Jun 13, 2023, 9:03 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     unsigned char buffer[block_size];
     // Iterate through the input file audio data
     // Read in each block of auditory data starting from the very end of the input file and moving backwards
-    while (fseek(input_file, 0, SEEK_END); ftell(input_file) > sizeof(header) - block_size; fseek(input_file, 0 - (2 * block_size), SEEK_CUR) != 528)
+    while (fseek(input_file, 0, SEEK_END)); ftell(input_file) > sizeof(header) - block_size; fseek(input_file, 0 - (2 * block_size), SEEK_CUR) != 528)
     {
         // Simultaneously writing each block to the output file so they are written in reverse order.
         fread(buffer, block_size, 1, input_file);
