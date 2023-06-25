d2a524aa21716c5166d05ca56a4d22df3c6e7607
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-25 10:01:27 +0000

    Sun, Jun 25, 2023, 11:01 AM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -74,7 +74,7 @@ bool load(const char *dictionary)
         return false;
     }
 
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
