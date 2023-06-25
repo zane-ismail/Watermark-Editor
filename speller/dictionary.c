a107cf93aec505ade6f127f6a420f3ad23ce97be
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-25 10:29:49 +0000

    Sun, Jun 25, 2023, 11:29 AM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -56,14 +57,15 @@ bool load(const char *dictionary)
             return false;
 
         }
-        strcopy(n->word, tmp);
-
+        strcpy(n->word, tmp);
     }
 
-
     // Hash word to obtain hash function
+    hash(n);
 
     // Insert word into hash table at that function
+
+
     return false;
 }
 
