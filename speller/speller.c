d67cfb183a669a46ffa5efbcbbff3a8dfa715fde
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-26 10:47:20 +0000

    Mon, Jun 26, 2023, 11:47 AM +01:00

--- speller/speller.c
+++ speller/speller.c
@@ -193,5 +193,4 @@ double calculate(const struct rusage *b, const struct rusage *a)
                   (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
                 / 1000000.0);
     }
-printf("Hello world!");
 }
