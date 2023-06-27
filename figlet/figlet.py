02c58ec17d97385f1ee78cafe0c09dd0492256dd
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-27 03:59:21 +0000

    Tue, Jun 27, 2023, 4:59 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,22 +2,21 @@ from pyfiglet import Figlet
 import sys
 import random
 
-if len(sys.argv) == 1 or len(sys.argv) == 3:
+figlet = Figlet()
+
+if len(sys.argv) == 3:
     input = input("Input: ")
-    figlet = Figlet()
-
-    if len(sys.argv) == 3:
-        if sys.argv[1] == "-f" or sys.argv[2] == "--font":
-            figlet.setFont(font=sys.argv[2])
-        else:
-            sys.exit
+    if sys.argv[1] == "-f" or sys.argv[2] == "--font":
+        figlet.setFont(font=sys.argv[2])
     else:
-        figlet.setFont(font=random.choice(figlet.getFonts()))
-
-    print(f"Output: {figlet.renderText(input)}")
+        sys.exit
+elif len(sys.argv) == 1:
+    input = input("Input: ")
+    figlet.setFont(font=random.choice(figlet.getFonts()))
 else:
     print("Invalid usage")
     sys.exit
+print(f"Output: {figlet.renderText(input)}")
 
 
 
