aa1534f79d4ec13314eb5a62b7ed12d52579f989
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-27 04:15:36 +0000

    Tue, Jun 27, 2023, 5:15 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,20 +9,23 @@ if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         try:
             figlet.setFont(font=sys.argv[2])
-        except NameError:
+            print(f"Output: {figlet.renderText(input)}")
+        except:
             print("Font not found ")
+            sys.exit
     else:
         sys.exit
 
 elif len(sys.argv) == 1:
     input = input("Input: ")
     figlet.setFont(font=random.choice(figlet.getFonts()))
+    print(f"Output: {figlet.renderText(input)}")
 
 else:
     print("Invalid usage")
     sys.exit
 
-print(f"Output: {figlet.renderText(input)}")
+
 
 
 
