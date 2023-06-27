78bf888dd15f8fa626c04258d9bb7e1573cace57
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-27 03:26:01 +0000

    Tue, Jun 27, 2023, 4:26 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,12 @@
 from pyfiglet import Figlet
-if argc == 0 or argc == 2:
+import sys
+
+if sys.argc == 0 or sys.argc == 2:
     input = input("Input: ")
     figlet = Figlet()
     figlet.getFonts()
     figlet.setFont(font="5lineoblique")
-    if argv[2] == "-f" or argv[2] == "--font":
+    if sys.argv[2] == "-f" or sys.argv[2] == "--font":
         print(f"Output: {figlet.renderText(input)}")
 
 
