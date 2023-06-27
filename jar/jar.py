31096dd123b8b03f6144079576e7f2acbf24d002
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-27 04:56:11 +0000

    Tue, Jun 27, 2023, 5:56 AM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,24 @@
+def main():
+    jar = Jar()
+    print(str(jar.capacity))
+
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        pass
 
     def __str__(self):
-        ...
+        return("🍪")
 
     def deposit(self, n):
-        ...
+        jar.append(n)
 
     def withdraw(self, n):
-        ...
+        jar.remove(n)
 
     @property
     def capacity(self):
-        ...
+        return self.capacity
 
     @property
     def size(self):
-        ...
+        return self.size
