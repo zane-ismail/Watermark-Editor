29a6dd9e8e74317934e2c2bb61bf65335a5b42a3
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-27 03:53:08 +0000

    Tue, Jun 27, 2023, 4:53 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -11,8 +11,8 @@ if len(sys.argv) == 1 or len(sys.argv) == 3:
         if sys.argv[1] == "-f" or sys.argv[2] == "--font":
             figlet.setFont(font=sys.argv[2])
     else:
-        figlet.setFont(font=random.choice(['basic', 'doom', 'moo']))
-        
+        figlet.setFont(font=random.choice(['basic', 'doom']))
+
     print(f"Output: {figlet.renderText(input)}")
 else:
     print("Invalid usage")
