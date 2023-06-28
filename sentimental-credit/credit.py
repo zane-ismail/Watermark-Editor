54fac45b511ebd31fb483daabde9c38793929535
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-28 06:54:56 +0000

    Wed, Jun 28, 2023, 7:54 AM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,4 +1,28 @@
 # TODO
+def main()
+    card = get_card_number()
+    card_length = get_card_length(card)
+    print(card)
 
-def main():
-    // Get card number
+
+# Get card number
+def get_card_number():
+    card_number = int(input("Card number: "))
+    return card_number
+
+
+# Get card number length
+def get_card_length(card):
+    length = len(card)
+
+    # Reject cards with less than 13 or more than 16 numbers
+    if length < 13 or length > 16:
+        print("INVALID")
+
+    return length
+
+
+main()
+# Luhn's algorithm
+# def validate_card(card, length)
+
