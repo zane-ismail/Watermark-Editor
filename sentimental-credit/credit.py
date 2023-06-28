e9b7e19300351661af53b4d49bb1c49f14081844
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-06-28 07:11:48 +0000

    Wed, Jun 28, 2023, 8:11 AM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -27,10 +27,9 @@ def get_card_length(card):
 def validate_card(card, length):
     print(card)
     # Iterate to find the second to last digit and every other digit after that
-    for digit in card:
-        for i in range(0, length, 2):
-            print(digit)
-            sum = digit * 2
+    for i in range(0, length, 2) in card:
+        print(digit)
+        sum = digit * 2
 
     return
 
