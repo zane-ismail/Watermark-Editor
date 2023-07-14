b0681e630970f1dbfb9dee6a3ba132b1bfc29006
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 21:16:04 +0000

    Fri, Jul 14, 2023, 10:16 PM +01:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,24 +1,24 @@
-CREATE TABLE assignments (
-    id INTEGER NOT NULL,
-    student_id INTEGER NOT NULL,
-    house_id INTEGER NOT NULL,
-    PRIMARY KEY(id)
-);
+    CREATE TABLE assignments (
+        id INTEGER NOT NULL,
+        student_id INTEGER NOT NULL,
+        house_id INTEGER NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-Create TABLE houses (
-    id INTEGER NOT NULL,
-    house_name TEXT NOT NULL,
-    head TEXT NOT NULL,
-    PRIMARY KEY(id)
-);
+    Create TABLE houses (
+        id INTEGER NOT NULL,
+        house_name TEXT NOT NULL,
+        head TEXT NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-CREATE TABLE students (
-    id INTEGER NOT NULL,
-    student_name TEXT NOT NULL,
-    house TEXT NOT NULL,
-    head TEXT NOT NULL,
-    PRIMARY KEY(id)
-);
+    CREATE TABLE students (
+        id INTEGER NOT NULL,
+        student_name TEXT NOT NULL,
+        house TEXT NOT NULL,
+        head TEXT NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-EXEC sp_rename 'students', 's';
-ALTER TABLE 'students' RENAME TO 's';
+    EXEC sp_rename 'students', 's';
+    ALTER TABLE 'students' RENAME TO 's';
