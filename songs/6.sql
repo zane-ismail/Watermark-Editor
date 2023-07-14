0fe3ea5d19f200cd58eec0502355900232eac422
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 21:47:28 +0000

    Fri, Jul 14, 2023, 10:47 PM +01:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,3 +1,6 @@
-    In 6.sql, write a SQL query that lists the names of songs that are by Post Malone.
-        Your query should output a table with a single column for the name of each song.
-        You should not make any assumptions about what Post Malone’s artist_id is.
+In 6.sql, write a SQL query that lists the names of songs that are by Post Malone.
+    Your query should output a table with a single column for the name of each song.
+    You should not make any assumptions about what Post Malone’s artist_id is.
+
+SELECT name FROM songs
+    WHERE artist_id = (SELECT)
