b6a41c9dbbeefd9e84bd6aa87c565f7e386680c9
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 15:42:28 +0000

    Fri, Jul 14, 2023, 4:42 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -1,8 +1,10 @@
 import csv
 
+db = SQL.("sqlite:///students.db")
+
 with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     counts = {}
     for row in reader:
         print(row["student_name"])
-        print(students.db)
+        db.execute("INSERT INTO * FROM colors")
