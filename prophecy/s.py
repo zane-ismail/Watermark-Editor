11ff7d6d7a2a3d6f1246aa73d701cdae82570275
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 21:09:10 +0000

    Fri, Jul 14, 2023, 10:09 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -35,14 +35,14 @@ with open("students.csv", "r") as file:
         create_relationships(relationships, name, house)
     print(houses, students, relationships)
 
-for student in students:
-    print(student)
-    db.execute("INSERT INTO students (student_name) VALUES ?", student["student_name"])
+# for student in students:
+#     print(student)
+#     db.execute("INSERT INTO students (student_name) VALUES (?)", student["student_name"])
 
 for rel in relationships:
     print(rel)
-    db.execute("INSERT INTO relationship (student_name, house) VALUES (?,?)", rel["student_name"], rel["house"])
+    db.execute("INSERT INTO assignments (student_name, house) VALUES (?,?)", rel["student_name"], rel["house"])
 
-for house in houses:
-    print(house)
-    db.execute("INSERT INTO houses (house, head) VALUES (?,?)", house["house"], head["head"])
+# for house in houses:
+#     print(house)
+#     db.execute("INSERT INTO houses (house, head) VALUES (?,?)", house["house"], head["head"])
