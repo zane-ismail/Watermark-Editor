d9a821aa571bdf59eda7ca8aa831c77266874e37
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 13:38:04 +0000

    Fri, Jul 14, 2023, 2:38 PM +01:00

---
+++ prophecy/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE houses (
+    id INTEGER,
+    house_name TEXT,
+    head TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE students (
+    id INTEGER,
+    student_name TEXT,
+    house TEXT,
+    head TEXT,
+    PRIMARY KEY(id)
+);
+
