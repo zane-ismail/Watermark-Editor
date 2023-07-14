9a2d7449cd5f7c4940999b3466d5226ae36caebb
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 13:13:52 +0000

    Fri, Jul 14, 2023, 2:13 PM +01:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -8,4 +8,4 @@ UPDATE shows SET title = "The Office" WHERE title = "ThE OffiCE";
 UPDATE shows SET title = "The Office" WHERE title LIKE "%ffice";
 UPDATE shows SET title = "Game of Thrones" WHERE title = "GoT";
 UPDATE shows SET title = "Adventure Time" WHERE title LIKE "%dventure";
-UPDATE shows SET title = "Arrow" WHERE title LIKE "%"
+UPDATE shows SET title = "Arrow" WHERE title LIKE "rrow";
