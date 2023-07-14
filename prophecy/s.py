16ab9a94467f379885b1adf32dfb26f25b58c62d
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 15:28:36 +0000

    Fri, Jul 14, 2023, 4:28 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -1,7 +1,7 @@
 import csv
 
 with open('students.csv', newline='') as csvfile:
-    s = csv.reader(csvfile)
+    reader = csv.DictReader(csvfile)
 
     for student in s['student_name']:
         print(student)
