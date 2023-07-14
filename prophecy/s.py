6dd7d8b927d2e96382b357ac699381c1f84d09fb
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 15:57:07 +0000

    Fri, Jul 14, 2023, 4:57 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -8,4 +8,4 @@ with open("students.csv", "r") as file:
     counts = {}
     for row in reader:
         print(row["student_name"])
-        db.execute("INSERT INTO students VALUES (student_name, row['student_name']");
+        db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']}");
