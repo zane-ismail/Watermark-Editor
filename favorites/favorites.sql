2b152f65c1f4d0e605e301460ee91930cca4637f
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 13:18:52 +0000

    Fri, Jul 14, 2023, 2:18 PM +01:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,11 +1,22 @@
 UPDATE shows SET title = "" WHERE title LIKE "%"
+UPDATE shows SET title = "Arrow" WHERE title LIKE "rrow";
+UPDATE shows SET title = "Adventure Time" WHERE title LIKE "%dventure";
 UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Broo%";
 UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "Game%";
 UPDATE shows SET title = "Grey’s Anatomy" WHERE title LIKE "Grey%";
 UPDATE shows SET title = "It’s Always Sunny in Philadelphia" WHERE title LIKE "%Philadelphia";
-UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Par;ks%";
+UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Parks%";
 UPDATE shows SET title = "The Office" WHERE title = "ThE OffiCE";
 UPDATE shows SET title = "The Office" WHERE title LIKE "%ffice";
 UPDATE shows SET title = "Game of Thrones" WHERE title = "GoT";
-UPDATE shows SET title = "Adventure Time" WHERE title LIKE "%dventure";
-UPDATE shows SET title = "Arrow" WHERE title LIKE "rrow";
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "%irbender";
+UPDATE shows SET title = "Community" WHERE title LIKE "%munity";
+UPDATE shows SET title = "Family Guy" WHERE title = "family guy";
+UPDATE shows SET title = "Friends" WHERE title LIKE "%iends";
+UPDATE shows SET title = "Gilmore Girls" WHERE title LIKE "%ilmore";
+UPDATE shows SET title = "How I Met Your Mother" WHERE title LIKE "%";
+UPDATE shows SET title = "" WHERE title LIKE "%";
+UPDATE shows SET title = "" WHERE title LIKE "%";
+UPDATE shows SET title = "" WHERE title LIKE "%";
+UPDATE shows SET title = "" WHERE title LIKE "%";
+UPDATE shows SET title = "" WHERE title LIKE "%";
