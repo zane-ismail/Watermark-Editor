649d1d9a11c7bf3d2f7b2dc0bc9883618e123e83
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 20:31:18 +0000

    Fri, Jul 14, 2023, 9:31 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -2,15 +2,29 @@ import csv
 from cs50 import SQL
 
 db = SQL("sqlite:///roster.db")
+students = []
+houses = []
+relationships = []
+
+
+def create_house(house, houses, head):
+    for h in houses:
+        if h["house"] == house:
+
+
+
 
 with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     counts = {}
     for row in reader:
-        print(row["student_name"])
-        print(row)
+        name = row["student_name"]
+        house = row["house"]
+        head = row["head"]
+
+        create_house(house, houses, head)
+
         # rows = db.execute("SELECT * FROM students")
-        db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']})");
-        print(row)
+        # db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']})");
         # if row['head'] not in row:
         #     db.execute(f"INSERT INTO students VALUES (house, {row['head']}");
