4457160cd1b182b6ad4eb346bdc3d56c73ec2efe
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 15:34:09 +0000

    Fri, Jul 14, 2023, 4:34 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -3,5 +3,6 @@ import csv
 with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     counts = {}
-    for student in reader['student_name']:
-        print(student)
+    for row in reader:
+        for student in row["student_name"]:
+            print(student)
