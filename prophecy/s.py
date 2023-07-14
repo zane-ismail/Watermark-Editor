d0d862491c0b0fd23ca91ef8bf5b631873c0bdd4
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 20:07:38 +0000

    Fri, Jul 14, 2023, 9:07 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -11,7 +11,7 @@ with open("students.csv", "r") as file:
 
         rows = db.execute("SELECT * FROM students")
         for r in rows:
-            insert = db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']}")
-            print(rows)
+            db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']}")
+            print(r)
         # if row['head'] not in row:
         #     db.execute(f"INSERT INTO students VALUES (house, {row['head']}");
