45420260e48529f7589004383d59689343bbd2ae
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 21:39:56 +0000

    Fri, Jul 14, 2023, 10:39 PM +01:00

--- songs/2.sql
+++ songs/2.sql
@@ -1,3 +1,3 @@
+In 2.sql, write a SQL query to list the names of all songs in increasing order of tempo.
+    Your query should output a table with a single column for the name of each song.
 
-    In 2.sql, write a SQL query to list the names of all songs in increasing order of tempo.
-        Your query should output a table with a single column for the name of each song.
