6f22c5eb84dd995234dde160e08d6d575475009c
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 22:08:12 +0000

    Fri, Jul 14, 2023, 11:08 PM +01:00

--- songs/2.sql
+++ songs/2.sql
@@ -1,5 +1,1 @@
-In 2.sql, write a SQL query to list the names of all songs in increasing order of tempo.
-    Your query should output a table with a single column for the name of each song.
-
-SELECT name FROM songs
-    ORDER BY tempo;
+SELECT name FROM songs ORDER BY tempo;
--- songs/5.sql
+++ songs/5.sql
@@ -1,4 +1,1 @@
-In 5.sql, write a SQL query that returns the average energy of all the songs.
-    Your query should output a table with a single column and a single row containing the average energy.
-
 SELECT AVG(energy) FROM songs;
