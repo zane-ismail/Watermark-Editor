91e1924aca82761d7c6fcf89b633aadb859df0c8
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 21:05:38 +0000

    Fri, Jul 14, 2023, 10:05 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -36,6 +36,8 @@ with open("students.csv", "r") as file:
     print(houses, students, relationships)
 
 for student in students:
-    db.execute(f"INSERT INTO students VALUES (student_name, {student['student_name']})");
+    db.execute("INSERT INTO students (student_name) VALUES ?", student["student_name"])
         # if row['head'] not in row:
+for rel in relationships:
+    db.execute(f"INSERT INTO relationship VALUES (student_name, house){students['student_name']})")
         #     db.execute(f"INSERT INTO students VALUES (house, {row['head']}");
