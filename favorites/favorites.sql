2262b9465b1aea1adb111a65c56b039982e1cd62
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 13:09:18 +0000

    Fri, Jul 14, 2023, 2:09 PM +01:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,9 +1,9 @@
 UPDATE shows SET title = "" WHERE title LIKE "%"
-UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Broo%"
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "Broo%";
 UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "Game%"
 UPDATE shows SET title = "Grey’s Anatomy" WHERE title LIKE "Grey%"
-UPDATE shows SET title = "It’s Always Sunny in Philadelphia" WHERE title LIKE "%Philadelphia"
-UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Parks%"
+UPDATE shows SET title = "It’s Always Sunny in Philadelphia" WHERE title LIKE "%Philadelphia";
+UPDATE shows SET title = "Parks and Recreation" WHERE title LIKE "Par;ks%";
 UPDATE shows SET title = "The Office" WHERE title = "ThE OffiCE"
 UPDATE shows SET title = "" WHERE title LIKE "%"
 UPDATE shows SET title = "" WHERE title LIKE "%"
