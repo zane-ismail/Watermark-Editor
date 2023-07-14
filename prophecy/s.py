d41530cdf22fe6c7fac5cf917a516caa12e04103
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-14 16:02:19 +0000

    Fri, Jul 14, 2023, 5:02 PM +01:00

--- prophecy/s.py
+++ prophecy/s.py
@@ -7,10 +7,10 @@ with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     counts = {}
     for row in reader:
-        print(row["student_name"])
+        # print(row["student_name"])
 
-        print(row["student_name"])
-        db.execute("SELECT * FROM students");
-        db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']}");
-        if row['head'] not in row:
-            db.execute(f"INSERT INTO students VALUES (house, {row['head']}");
+        db = db.execute("SELECT * FROM students")
+        print(db)
+        # db.execute(f"INSERT INTO students VALUES (student_name, {row['student_name']}");
+        # if row['head'] not in row:
+        #     db.execute(f"INSERT INTO students VALUES (house, {row['head']}");
