638735093da1e4b3b5783d5c316b1324e65e3b53
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 15:29:01 +0000

    Sat, Jul 15, 2023, 4:29 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND description LIKE "%duck%";
