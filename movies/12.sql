6825250fbb3832e18767d098036dd5135049a19b
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 13:21:21 +0000

    Sat, Jul 15, 2023, 2:21 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -10,4 +10,4 @@ SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id
 SELECT title FROM movies WHERE id IN
 (SELECT id FROM movies WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id IN
-(SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence")) HAVING COUNT(id) > 1);
+(SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence") HAVING COUNT(person_id) > 1));
