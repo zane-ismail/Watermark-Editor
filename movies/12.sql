8b5648bf3e99674c970b5545739ba105a1846a12
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 14:55:25 +0000

    Sat, Jul 15, 2023, 3:55 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,13 +1,7 @@
-In 12.sql, write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
-
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Bradley Cooper.
-    You may assume that there is only one person in the database with the name Jennifer Lawrence.
-
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence"));
-
-
 SELECT title FROM movies WHERE id IN
 (SELECT id FROM movies WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id IN
-(SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence")));
+(SELECT id FROM people WHERE name = "Bradley Cooper")))
+AND id IN
+(SELECT movie_id FROM stars WHERE person_id IN
+(SELECT id FROM people WHERE name = "Jennifer Lawrence"));
