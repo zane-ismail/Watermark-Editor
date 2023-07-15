a90f8e2b2d52ebe8535722c3b656034efbd10159
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 13:48:21 +0000

    Sat, Jul 15, 2023, 2:48 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating LIMIT 5 ;
+SELECT title FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
