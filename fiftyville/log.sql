50ecfeb7c336d1e2216d5c55319777a6c8801448
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 17:22:50 +0000

    Sat, Jul 15, 2023, 6:22 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,8 +32,7 @@ SELECT name FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE
 year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
 AND id IN
-(SELECT person_id FROM bank_accounts WHERE person_id IN
-(SELECT id FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street"))
+(SELECT id FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street")
 AND phone_number IN
 (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60)
 AND passport_number IN
