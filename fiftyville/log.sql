8a35ff15995b16947544ecc5906028b2ce2ef2fc
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 17:25:19 +0000

    Sat, Jul 15, 2023, 6:25 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,7 +32,8 @@ SELECT name FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE
 year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
 AND id IN
-(SELECT id FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street")
+(SELECT person_id FROM bank_accounts WHERE account_number IN
+(SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street"))
 AND phone_number IN
 (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60)
 AND passport_number IN
@@ -43,13 +44,19 @@ AND passport_number IN
 -- Find the accomplice
 SELECT name FROM people WHERE phone_number =
 (SELECT receiver FROM phone_calls WHERE caller =
-(SELECT phone_number FROM people WHERE license_plate IN
+SELECT phone_number FROM people WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE
-year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25
-) AND phone_number IN
-(SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60)
+year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
+AND id IN
+(SELECT person_id FROM bank_accounts WHERE account_number IN
+(SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street"))
+AND phone_number IN
+(SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60)
 AND passport_number IN
+(SELECT passport_number FROM passengers WHERE flight_id =
+(SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id =
+(SELECT ID FROM airports WHERE city = "Fiftyville") ORDER BY flights.hour));
 (SELECT passport_number FROM passengers WHERE flight_id =
 (SELECT id FROM flights WHERE year = 2021 AND month = 7 AND day = 29 AND origin_airport_id IN
 (SELECT ID FROM airports WHERE city = "Fiftyville")))
-AND year = 2021 AND month = 7 AND day = 28 AND duration < 60));
+AND year = 2021 AND month = 7 AND day = 28 AND duration < 60);
