3f905c012e69e5733d7a7ca06505b59f4243c525
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 13:49:12 +0000

    Sat, Jul 15, 2023, 2:49 PM +01:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 SELECT name FROM people WHERE id IN
 (SELECT person_id FROM stars WHERE movie_id IN
 (SELECT movie_id FROM stars WHERE person_id IN
-(SELECT id from people WHERE name = "Kevin Bacon")));
+(SELECT id from people WHERE name = "Kevin Bacon" AND birth = 1958)));
