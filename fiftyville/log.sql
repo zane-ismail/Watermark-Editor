934506fffd91383978f3f367ec47052e66b71d78
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 15:38:42 +0000

    Sat, Jul 15, 2023, 4:38 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,9 +8,12 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street" AND 
 SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
 --Look at bakery_security_logs for cars that left the parking lot within 10 mins of theft
-
+SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28;
 
 --Look at atm_transactions in the morning before the theft for someone Eugene recognised
 
 
---Look at phone_calls after the 
+--Look at phone_calls lasting less than a minute around the time of the theft
+
+
+--Look at flights for the earliest flight from fiftyville on 29/07/2021
