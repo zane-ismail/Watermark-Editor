529a39e29272158d7a9c9238ea5aa319f97e10bc
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 15:41:52 +0000

    Sat, Jul 15, 2023, 4:41 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,7 @@ SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transc
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 
 --Look at atm_transactions in the morning before the theft for someone Eugene recognised
-
+SELECT
 
 --Look at phone_calls lasting less than a minute around the time of the theft
 
