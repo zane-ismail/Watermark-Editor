b245353c9a16cd6cf6301efd734755120d2676e9
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 12:46:41 +0000

    Sat, Jul 15, 2023, 1:46 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,4 +3,4 @@ In 12.sql, write a SQL query to list the titles of all movies in which both Brad
     Your query should output a table with a single column for the title of each movie.
     You may assume that there is only one person in the database with the name Bradley Cooper.
     You may assume that there is only one person in the database with the name Jennifer Lawrence.
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence") CASE WHEN count());
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper" OR name = "Jennifer Lawrence") CASE WHEN);
