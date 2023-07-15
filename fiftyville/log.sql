15976d860a419e7bc1bdabc394a7cbf9181eb892
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 15:46:26 +0000

    Sat, Jul 15, 2023, 4:46 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,10 +11,10 @@ SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transc
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 
 --Look at atm_transactions in the morning before the theft for someone Eugene recognised
-SELECT account_number FROM atm_transactions
-WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28;
 
 --Look at phone_calls lasting less than a minute around the time of the theft
-
+SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
 --Look at flights for the earliest flight from fiftyville on 29/07/2021
+SELECT destination_airport FROM flights WHERE origin_airport = "Fiftyville"
