b378884760bf62eea18474a35c38c6e5e62a3512
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-15 13:46:02 +0000

    Sat, Jul 15, 2023, 2:46 PM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT movies.title, ratings.rating FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id ORDER BY ratings.rating DESC;
+SELECT movies.title, ratings.rating FROM movies INNER JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY ratings.rating DESC;
