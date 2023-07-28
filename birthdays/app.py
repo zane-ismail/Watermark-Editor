9ea4e43a6818a9b0c8fdd7358704b1ca3db09b6e
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-28 19:03:36 +0000

    Fri, Jul 28, 2023, 8:03 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -36,7 +36,7 @@ def index():
         else:
             message = "Input incomplete"
             rows = db.execute("SELECT * FROM birthdays")
-            return render_template("index.html",message=message, rows=rows)
+            return render_template("index.html", message=message, rows=rows)
 
     else:
         # TODO: Display the entries in the database on index.html
@@ -51,10 +51,9 @@ def delete(id):
         message = "Entry deleted"
         print(id)
         print("????????")
-        db.execute("DELETE FROM birthdays WHERE id = ?)", [request.form["id"]])
-        db.commit()
+        db.execute("DELETE FROM birthdays WHERE id = ?", id)
         rows = db.execute("SELECT * FROM birthdays")
-        return redirect("index.html",message=message, rows=rows)
+        return redirect("index.html", message=message, rows=rows)
 
 
 
