ff9b1873239bfcf11ec0dc63d7694c99e303ccef
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-28 11:01:23 +0000

    Fri, Jul 28, 2023, 12:01 PM +01:00

--- helloflask/app.py
+++ helloflask/app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if requests.method == "GET":
+        return render_template("index.html")
+    else:
+        print("Form submitted!")
+        color = request.form.get("color")
+        return render_template("color.html", color=color)
