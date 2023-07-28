a7dd30e5b3547199b743b21c801b74305096fef1
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-28 18:24:20 +0000

    Fri, Jul 28, 2023, 7:24 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -35,8 +35,8 @@ def index():
             return redirect("/")
 
         if request.form['action'] == 'delete':
-            # d_name = request.form.get("name")
-            # db.execute("DELETE FROM birthdays WHERE name = ?", d_name)
+            id = request.form.get("id")
+            db.execute("DELETE FROM birthdays WHERE name = ?", id)
             return redirect("/")
 
     else:
