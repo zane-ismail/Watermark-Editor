1eb1864269ff3810dc236916c450ef0fefc8e15a
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-28 15:21:06 +0000

    Fri, Jul 28, 2023, 4:21 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        rows = db.execute("SELECT * FROM db;")
+        db.execute("SELECT * FROM db;")
         return render_template("index.html", rows)
 
 
