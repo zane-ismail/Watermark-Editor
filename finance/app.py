dc223e4f29d952c85e8e5ffee6e5b8a4b09f166a
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-29 15:36:41 +0000

    Sat, Jul 29, 2023, 4:36 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -124,16 +124,17 @@ def register():
 
     # Render an apology if either input is blank or the passwords do not match.
 
-    if username:
-        if password and confirmation == password:
-            # store a hash of the user’s password, not the password itself
-            hash = password
-            hash = generate_password_hash(password)
-            # INSERT the new user into users
+    if username and password and confirmation == password:
+        # store a hash of the user’s password, not the password itself
+        hash = password
+        hash = generate_password_hash(password)
+        # INSERT the new user into users
+        try:
             db.execute("INSERT into users (username, hash) VALUES (?, ?)", username, hash)
+        except ValueError:
+            return render_template("apology.html")
     else:
-        print("ERROR")
-        return render_template("apology.html")
+        return render_template("register.html")
     return render_template("register.html")
 
 
