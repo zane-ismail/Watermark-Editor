db346376bb6d95d2e7fd0b99b72a5cf9fe6414ca
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-31 07:22:50 +0000

    Mon, Jul 31, 2023, 8:22 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -48,7 +48,7 @@ def buy():
         # Require that a user input a stock’s symbol, implemented as a text field whose name is symbol.symbol = request.form.get("symbol")
         symbol = request.form.get("symbol")
         # Require that a user input a number of shares, implemented as a text field whose name is shares.
-        int(shares) = request.form.get("shares")
+        shares = int(request.form.get("shares"))
         print(shares)
         symbol = lookup(symbol)
         # Render an apology if the input is blank or the symbol does not exist (as per the return value of lookup).
