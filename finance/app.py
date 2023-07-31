af92c926997ee09fb7ee04cfd287babd7d6df7fb
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-07-31 07:36:59 +0000

    Mon, Jul 31, 2023, 8:36 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -59,8 +59,8 @@ def buy():
             # Call lookup to look up a stock’s current price
             price = lookup(symbol)
             # SELECT how much cash the user currently has in users
-            cash = db.execute("SELECT cash FROM users WHERE username = ?", username)
-            print(cash)
+            # cash = db.execute("SELECT cash FROM users WHERE username = ?")
+            # print(cash)
 
             # Upon completion, redirect the user to the home page.
             return redirect("/")
