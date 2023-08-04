4e0622ff23ce47075c5241b4954dda84fde59df3
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-08-04 10:55:21 +0000

    Fri, Aug 4, 2023, 11:55 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -217,6 +217,7 @@ def sell():
         if total_shares < shares:
             return apology("Not enough stocks")
         elif symbol == user_stocks:
+            db.execute("INSERT INTO purchases VALUES (?, ?, ?, ?)", session['user_id'], symbol, shares, price, transaction)
             return redirect("/")
             # Render an apology if the user fails to select a stock
         elif not symbol:
