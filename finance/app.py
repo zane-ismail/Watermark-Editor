fdc6d5e283d23b03eac11b9a25240f0512d653d5
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-08-06 03:25:09 +0000

    Sun, Aug 6, 2023, 4:25 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -72,8 +72,7 @@ def buy():
                 # Add one or more new tables to finance.db via which to keep track of the purchase.
                 db.execute("CREATE TABLE purchases(user_id int NOT NULL, symbol varchar(255), shares int, price float, type varchar(4))")
                 # Store enough information so that you know who bought what at what price and when.
-                # except:
-                #     pass
+                cash = cash - 
 
             db.execute("INSERT INTO purchases VALUES (?, ?, ?, ?, ?)", session['user_id'], symbol, shares, price, transaction)
             # Upon completion, redirect the user to the home page.
