df2d8c14d6e14377e4c8ea4106646f1b7d025b78
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-09-12 04:49:27 +0000

    Tue, Sep 12, 2023, 5:49 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
@@ -7,6 +8,8 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
+
+ts = time.time()
 
 # Configure application
 app = Flask(__name__)
