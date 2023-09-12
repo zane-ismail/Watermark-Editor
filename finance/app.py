781750d5ceb1a582c45cbecbcc15d2092868746a
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-09-12 04:53:48 +0000

    Tue, Sep 12, 2023, 5:53 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -1,6 +1,6 @@
 import os
-import time
 
+from datetime import datetime
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -9,7 +9,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
 
-ts = time.time()
+ts = datetime.today().strftime('%Y/%-m/%-d')
 
 # Configure application
 app = Flask(__name__)
