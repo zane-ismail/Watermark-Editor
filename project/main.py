1c6da5ce1d3ae26fba11eb50c6d4a7b2ffac523c
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-10-04 06:30:57 +0000

    Wed, Oct 4, 2023, 7:30 AM +01:00

--- project/main.py
+++ project/main.py
@@ -54,8 +54,8 @@ def create_database():
     try:
         # Add one or more new tables to finance.db via which to keep track of the purchase.
         # Store enough information so that you know who bought what at what price and when.
-        db.execute("CREATE TABLE recipes(id int NOT NULL, name varchar(255), ingredients varchar(255), description varchar(255),  steps text, minutes int, tags varchar(255), n_ingredients int, n_steps int"))
-        db.execute("INSERT INTO purchases VALUES (?, ?, ?, ?, ?, ?)", session['user_id'], symbol, shares, price, transaction, timestamp)
+        db.execute("CREATE TABLE recipes(id int NOT NULL, name varchar(255), ingredients varchar(255), description varchar(255), steps text, minutes int, tags varchar(255), n_ingredients int, n_steps int)")
+        db.execute("INSERT INTO purchases VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", session['user_id'], symbol, shares, price, transaction, timestamp)
     except RuntimeError:
         pass
 
