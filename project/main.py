8a7bdec5f47c9888c49b4fc30db6504d7542fccd
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-10-04 06:26:19 +0000

    Wed, Oct 4, 2023, 7:26 AM +01:00

--- project/main.py
+++ project/main.py
@@ -55,7 +55,7 @@ def read_csv():
 #         # Add one or more new tables to finance.db via which to keep track of the purchase.
 #         # Store enough information so that you know who bought what at what price and when.
 #         db.execute(
-#             "CREATE TABLE recipes(id int NOT NULL, name varchar(255), ingredients varchar(255), description varchar(255),  steps text, minutes int, tags varchar(255)")
+#             "CREATE TABLE recipes(id int NOT NULL, name varchar(255), ingredients varchar(255), description varchar(255),  steps text, minutes int, tags varchar(255), n_ingredients int, n_steps int")
 #     except RuntimeError:
 #         pass
 
