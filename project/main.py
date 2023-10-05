a9181d8430acfc5b40ca208b19a0daa2f17d81a5
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-10-05 23:41:10 +0000

    Fri, Oct 6, 2023, 12:41 AM +01:00

--- project/main.py
+++ project/main.py
@@ -60,10 +60,10 @@ def create_database():
     except:
         pass
     for i in range(len(data)):
-        data[i]['ingredients'] = data[i]['ingredients'].replace("[", " ").replace("]", " ").replace(",", ";")
-        data[i]['description'] = data[i]['description'].replace("[", " ").replace("]", " ").replace(",", ";")
-        data[i]['steps'] = data[i]['steps'].replace("[", " ").replace("]", " ").replace(",", ";")
-        data[i]['tags'] = data[i]['tags'].replace("[", " ").replace("]", " ").replace(",", ";")
+        data[i]['ingredients'] = data[i]['ingredients'].replace("[", " ").replace("]", " ").replace(",", ";").replace("'", " ")
+        data[i]['description'] = data[i]['description'].replace("[", " ").replace("]", " ").replace(",", ";").replace("'", " ")
+        data[i]['steps'] = data[i]['steps'].replace("[", " ").replace("]", " ").replace(",", ";").replace("'", " ")
+        data[i]['tags'] = data[i]['tags'].replace("[", " ").replace("]", " ").replace(",", ";").replace("'", " ")
 
         print(data[i])
         # Todo Add csv into database
