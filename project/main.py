b4ad91d437ea1b1526ceff47108faadb1c4d8612
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-10-05 04:19:11 +0000

    Thu, Oct 5, 2023, 5:19 AM +01:00

--- project/main.py
+++ project/main.py
@@ -63,7 +63,8 @@ def create_database():
         data[i]['description'].replace("[", " ").replace("]", " ")
         data[i]['steps'].replace("[", " ").replace("]", " ")
         data[i]['tags'].replace("[", " ").replace("]", " ")
-        
+        print(data[i]['id'], data[i]['name'], data[i]['ingredients'], data[i]['description'], data[i]['steps'], data[i]['minutes'], data[i]['tags'], data[i]['n_ingredients'], data[i]['n_steps'])
+
         # Todo Add csv into database
         try:
             # Todo add each parameter to the db
