c8951e72fa373a3e1dec57e33f16579b19cbfd31
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-12-19 15:13:40 +0000

    Tue, Dec 19, 2023, 10:13 PM +07:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,8 @@
-SELECT DISTINCT people.name FROM people JOIN stars ON people.id= stars.person_id JOIN movies ON stars.movies.id WHERE movies.year = 2012 ORDER BY people.birth;
+In 9.sql, write a SQL query to list the names of all people who starred in a movie released in 2004, ordered by birth year.
+ SELECT DISTINCT people.name FROM people JOIN stars ON people.id= stars.person_id JOIN movies ON stars.movies.id WHERE movies.year = 2012 ORDER BY people.birth;
+    Your query should output a table with a single column for the name of each person.
+    People with the same birth year may be listed in any order.
+    No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
+    If a person appeared in more than one movie in 2004, they should only appear in your results once.
 
 
