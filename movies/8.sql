d3443db4d9171123082e4a758024146983a288fa
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-12-21 19:37:06 +0000

    Thu, Dec 21, 2023, 2:37 PM -05:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,4 @@
 
     Your query should output a table with a single column for the name of each person.
     You may assume that there is only one movie in the database with the title Toy Story.
-SELECT people.name FROM people JOIN directors ON people.id= directors.person_id JOIN movies ON directors.movies_id= movies.id WHERE movies.title = " Toy Story";
+SELECT people.name FROM people JOIN directors ON people.id= directors.person_id JOIN movies ON directors.movie_id= movies.id WHERE movies.title = " Toy Story";
