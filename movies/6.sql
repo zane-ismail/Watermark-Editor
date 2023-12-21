e7b55e9338d5ff66cd46b1e6214c9cca6a483f28
Zane Ismail <116204633+zane-ismail@users.noreply.github.com>
2023-12-21 18:35:57 +0000

    Thu, Dec 21, 2023, 1:35 PM -05:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,4 +1,6 @@
 In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
 
     Your query should output a table with a single column and a single row (not counting the header) containing the average rating.
- SELECT AVG(ratings.rating) FROM ratings JOIN movies ON ratings.movie_id= movies.id WHERE movies.year= 2012;
+SELECT AVG(ratings.rating) FROM ratings JOIN movies ON ratings.movie_id= movies.id WHERE movies.year= 2012;
+
+SELECT AVG(rating) FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012);
